fa48ea11347c21db71576195387149315723019d
jacksonzhai <764087238@qq.com>
2018-09-25 20:54:02 +0800

    Create MorrisInorderTraversalTree.java

---
+++ Java/MorrisInorderTraversalTree.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+ public class MorrisInorderTraversalTree {
+     public List<Integer> morrisInorderTree(TreeNode root){
+        List<Integer> list = new LinkedList<>();
+        TreeNode current = root;
+        while(current != null){
+            if(current.left == null){
+                list.add(current.val);
+                current = current.right;
+            }
+            else{
+                TreeNode pre = current.left;
+                while (pre.right!=null&&pre.right!=current){
+                    pre = pre.right;
+                }
+                if(pre.right == null){
+                    pre.right = current;
+                    current = current.left;
+                }
+                else{
+                    pre.right = null;
+                    list.add(current.val);
+                    current = current.right;
+                }
+
+            }
+
+
+        }
+        return list;
+     }
+ }
